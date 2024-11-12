082ada5a044e41028349cbbc23c7bbca711e7369
rbss16 <reddibhargavisrivalli@gmail.com>
2024-11-12 12:27:13 +0530

    Added backend folder

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Footer = () => {
+  return (
+    <div className='md:mx-10'>
+      <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10  mt-40 text-sm'>
+
+        <div>
+          <img className='mb-5 w-40' src={assets.logo} alt="" />
+          <p className='w-full md:w-2/3 text-gray-600 leading-6'>CARE Hospitals is one of India’s leading multi-specialty healthcare providers, known for its commitment to delivering quality medical care with compassion. With a network of advanced facilities and highly skilled professionals, CARE Hospitals offers a wide range of services, from routine health checks to complex surgeries. The hospital is equipped with cutting-edge technology and adheres to international standards to ensure the highest level of patient care. At CARE Hospitals, patients can expect comprehensive healthcare in a supportive environment, aimed at achieving the best outcomes and promoting long-term wellness.</p>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>COMPANY</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>Home</li>
+            <li>About us</li>
+            <li>Delivery</li>
+            <li>Privacy policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className='text-xl font-medium mb-5'>GET IN TOUCH</p>
+          <ul className='flex flex-col gap-2 text-gray-600'>
+            <li>123456789</li>
+            <li>carehos.123@gmail.com</li>
+          </ul>
+        </div>
+
+      </div>
+
+      <div>
+        <hr />
+        <p className='py-5 text-sm text-center'>Copyright 2024 @ Prescripto.com - All Right Reserved.</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default Footer
